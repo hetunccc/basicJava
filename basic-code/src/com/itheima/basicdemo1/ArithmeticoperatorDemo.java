00f4c086ea3636ea9c4c693690ebdee1af5e0a26
hetunccc <111727859+hetunccc@users.noreply.github.com>
2023-02-04 08:48:58 +0900

    Initial commit

---
+++ basic-code/src/com/itheima/basicdemo1/ArithmeticoperatorDemo.java
@@ -0,0 +1,27 @@
+package com.itheima.basicdemo1;
+
+public class ArithmeticoperatorDemo {
+    public static void main(String[] args) {
+        //+
+        System.out.println(2 + 3);
+        //-
+        System.out.println( 3- 2);
+        //*
+        System.out.println(2 * 2);
+
+        //小数が計算する場合
+        System.out.println(1.2 + 3.5);
+        System.out.println(9.2 - 1.2);
+        System.out.println(2.3 * 21.1);
+
+
+        //   /
+        System.out.println(10 / 2);
+        System.out.println(10 / 3);
+
+        //%
+        System.out.println(10%3);
+        //byte--short--int-^long--float--double
+        //
+    }
+}
